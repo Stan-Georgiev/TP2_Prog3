00e4c54333ea5f4ecea12c7c556853482cdb556a
2494186 <2494186@B110014G006>
2025-09-22 09:36:41 -0400

    Partie code Attraction

---
+++ TP2_Prog3/Attraction.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_Prog3
+{
+    internal class Attraction
+    {
+        public enum TypeAttraction
+        {
+            S = 0,
+            I = 1,
+            F = 2,
+            T = 3,
+            M = 4,
+            R = 5
+        }
+
+        private string _id;
+        private string _name;
+        private int _capacity;
+        private TypeAttraction _type { get; }
+
+        
+
+
+        public Attraction(string id, TypeAttraction type, string name, int capacity) {
+            this._id = id;
+            this._name = name;
+            this._capacity = capacity;
+            this._type = type;
+        }
+
+        public string getId()
+        {
+            return _id;
+        }
+
+        public string getName()
+        {
+            return _name;
+        }
+
+        public int getCapacity()
+        {
+            return _capacity;
+        }
+
+        public TypeAttraction getType()
+        {
+            return _type;
+        }
+    }
+}
---
+++ TP2_Prog3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace TP2_Prog3
+{
+    public static class Program
+    {
+        private static readonly Parc Parc = new();
+        private static readonly Map Map = new();
+        private static readonly GestionVisiteurs GestionVisiteurs = new(Parc);
+        private static void Afficher()
+        {
+            Thread.Sleep(1000);
+            AffichageConsole.Afficher(Parc, Map, GestionVisiteurs);
+        }
+        private static void TestEntrerVisiteur(Visiteur visiteur)
+        {
+            GestionVisiteurs.EntrerVisiteurDansParc(visiteur);
+            GestionVisiteurs.EntrerVisiteurDansFilAttente("M0002", visiteur);
+            Afficher();
+        }
+        private static void TestSortirVisiteur(Visiteur visiteur)
+        {
+            GestionVisiteurs.SortirVisiteurDuParc(visiteur);
+            Afficher();
+        }
+        public static void Main()
+        {
+            AffichageConsole.Afficher(Parc, Map, GestionVisiteurs);
+            var visiteur1 = new Visiteur("Nom 1");
+            TestEntrerVisiteur(visiteur1);
+            var visiteur2 = new Visiteur("Nom 2");
+            TestEntrerVisiteur(visiteur2);
+            var visiteur3 = new Visiteur("Nom 3");
+            TestEntrerVisiteur(visiteur3);
+            var visiteur4 = new Visiteur("Nom 4");
+            TestEntrerVisiteur(visiteur4);
+            for (var i = 1; i <= 4; i++)
+            {
+                GestionVisiteurs.EntrerVisiteurDansAttraction("M0002");
+                Afficher();
+            }
+            TestSortirVisiteur(visiteur3);
+            TestSortirVisiteur(visiteur4);
+            TestSortirVisiteur(visiteur2);
+            TestSortirVisiteur(visiteur1);
+            AffichageConsole.AfficherHistoriqueVisiteur(visiteur1);
+        }
+    }
+}
