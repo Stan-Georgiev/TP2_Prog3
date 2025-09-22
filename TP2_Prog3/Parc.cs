1224bd9eadee5767440902fdc05907ca7b3d3339
2494186 <2494186@B110014G006>
2025-09-22 09:47:15 -0400

    .

--- TP2_Prog3/Parc.cs
+++ TP2_Prog3/Parc.cs
@@ -9,5 +9,11 @@ namespace TP2_Prog3
     internal class Parc
     {
         /** Utilisation d'une linked list**/
+        LinkedList<Attraction> Park = new LinkedList<Attraction>();
+        Attraction m1 = new Attraction("M0001", 0,"Manège 1",4);
+
+        public void ajouterAttraction(Attraction a) {
+            Park­.AddFirst(a);
+        }
     }
 }
