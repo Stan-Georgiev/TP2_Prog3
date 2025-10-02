be0afb675ed4ca17b47ebd54732b0c0b620d5ad7
6298861 <6298861@cstjean.qc.ca>
2025-10-02 12:23:21 -0400

    a

--- TP2_Prog3/AffichageConsole.cs
+++ TP2_Prog3/AffichageConsole.cs
@@ -12,18 +12,7 @@ namespace TP2_Prog3
 
         public static void Afficher(Parc parc, Map map, GestionVisiteur visiteurs)
         {
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < map.GetHeigt(); i++)
-            {
-                if (map.map[i] == "")
-                {
-                    builder.Append("-----   ");
-                }
-                else
-                {
-                    builder.Append(map.map[i]);
-                }
-            }
+            
           
             Console.WriteLine($"{visiteurs.GetNbVisiteur()}visiteurs(s) présent(s) dans le parc.");
 
