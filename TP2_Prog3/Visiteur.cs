51e8ee3fb497c740138cf1bfee7cbb053bef013d
2494186 <2494186@B110014G006>
2025-10-02 10:54:01 -0400

    Code Partiel Visiteur

--- TP2_Prog3/Visiteur.cs
+++ TP2_Prog3/Visiteur.cs
@@ -8,5 +8,23 @@ namespace TP2_Prog3
 {
     internal class Visiteur
     {
+        private string _nom;
+        private LinkedList<string> _historique;
+
+        public Visiteur(string nom, LinkedList<string> historique)
+        {
+            this._nom = nom;
+            _historique = historique;
+        }
+
+        public void AjouterHistorique(string action)
+        {
+            _historique.AddLast(action);
+        }
+
+        public string GetNom()
+        {
+            return _nom;
+        }
     }
 }
