e95c412b1a8924baa43da822cf874b82967e67f6
2494186 <2494186@B110014G006>
2025-10-06 08:33:39 -0400

    .

--- TP2_Prog3/GestionVisiteur.cs
+++ TP2_Prog3/GestionVisiteur.cs
@@ -22,24 +22,24 @@ namespace TP2_Prog3
 
         public void EntrerVisiteurDansFilAttente(string attractionId, Visiteur visiteur)
         {
-            visiteur.AjouterHistorique($"{visiteur.GetNom} rentre dans la file de l'attraction {attractionId}");
+            visiteur.AjouterHistorique($"{visiteur.GetNom()} rentre dans la file de l'attraction {attractionId}");
         }
 
         public void EntrerVisiteurDansAttraction(string attractionId)
         {
-           
+            
         }
 
         public void EntrerVisiteurDansParc(Visiteur visiteur)
         {
             nbVisiteur++;
-            visiteur.AjouterHistorique($"{visiteur.GetNom} rentre dans le parc");
+            visiteur.AjouterHistorique($"{visiteur.GetNom()} rentre dans le parc");
         }
 
         public void SortirVisiteurDuParc(Visiteur visiteur)
         {
             nbVisiteur--;
-            visiteur.AjouterHistorique($"{visiteur.GetNom} sort du le parc");
+            visiteur.AjouterHistorique($"{visiteur.GetNom()} sort du parc");
         }
     }
 }
--- TP2_Prog3/obj/Debug/net8.0/TP2_Prog3.AssemblyInfo.cs
+++ TP2_Prog3/obj/Debug/net8.0/TP2_Prog3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP2_Prog3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+774d110bc8c45fdada2418a62e10bd8018fa8b70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58ad0484c1346d86a871a96d41d69b8890206ed9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP2_Prog3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP2_Prog3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
